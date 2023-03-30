1013a9b921370aeea2a9ad2ff1f55f11a026c9a1
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-03-30 10:51:53 -0300

    hello world /task

--- project-tasks/src/task/task.controller.ts
+++ project-tasks/src/task/task.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+
+    @Get()
+    helloworld(){
+        return 'helloworld';
+    }
+
+}
