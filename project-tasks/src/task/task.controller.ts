7020b472bf2702ae2ad3227e739166bed1b2d78f
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-04-07 14:09:15 -0300

    update task by id added

--- project-tasks/src/task/task.controller.ts
+++ project-tasks/src/task/task.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Param, Controller, Delete, Get, Post } from '@nestjs/common';
+import { Body, Param, Controller, Delete, Get, Post, Patch } from '@nestjs/common';
 import { TaskService } from './task.service'
-import { CreateTaskDto } from './dto/task.dto';
+import { CreateTaskDto, UpdateTaskDto } from './dto/task.dto';
 
 @Controller('task')
 export class TaskController {
@@ -13,10 +13,16 @@ export class TaskController {
     }
 
     @Post()
-    createTask(@Body() newTask: CreateTaskDto){
+    createTask(@Body() newTask: CreateTaskDto) {
         console.log(newTask);
         return this.taskSerivce.cerateTask(newTask.title, newTask.description);
     }
+
+    @Patch(':id')
+    updateTask(@Param('id') id: string, @Body() updatedFields: UpdateTaskDto) {
+        return this.taskSerivce.updateTaskById(id, updatedFields);
+    }
+
 
     @Delete(':id')
     deleteTask(@Param('id') id: string) {
--- project-tasks/src/task/task.service.ts
+++ project-tasks/src/task/task.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.entity';
 import { v4 } from 'uuid';
+import { UpdateTaskDto } from './dto/task.dto';
+import { error } from 'console';
 
 @Injectable()
 export class TaskService {
@@ -28,11 +30,25 @@ export class TaskService {
         this.tasks.push(task)
 
         return task
-     }
-    updateTaskById(id: String) {
-     }
+    }
 
-    deleteTaskById(id: string) {
+    getTaskById(id: string) {
+        return this.tasks.find(task => task.id === id)
+        
+    }
+    updateTaskById(id: string, updatedFields: UpdateTaskDto): Task { 
+        const task = this.getTaskById(id);
+        if(!task) {
+            return
+        };
+
+        const newTask = Object.assign(task, updatedFields)
+        this.tasks = this.tasks.map(task => task.id === id ? newTask : task)
+        return newTask;
+    }
+
+
+    deleteTaskById(id: string) {        
         this.tasks = this.tasks.filter(task => task.id != id)
      }
 
