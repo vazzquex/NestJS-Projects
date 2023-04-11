e33af8778f4b8133758a0632b3484e7ab2eff61e
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-04-11 14:39:25 -0300

    validation node params

--- project-tasks/src/task/dto/task.dto.ts
+++ project-tasks/src/task/dto/task.dto.ts
@@ -1,12 +1,30 @@
 import { TaskStatus } from "../task.entity";
+import { IsNotEmpty, IsOptional, IsString, MinLength, IsIn } from 'class-validator'
+
 
 export class CreateTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     title: string;
+
+
+    @IsString()
+    @IsNotEmpty()
     description: string;
 }
 
 export class UpdateTaskDto {
+    @IsString()
+    @IsOptional()
     title?: string;
+
+    @IsString()
+    @IsOptional()
     description?: string;
+
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.PENDING, TaskStatus.DONE, TaskStatus.IN_PROGRESS])
     status?: TaskStatus;
 }
