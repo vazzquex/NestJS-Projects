80d2eedabaed7f0654536f242c7988b5b61dd2cb
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-04-16 10:45:21 -0300

    fix type error

--- mongodb-products-api/src/app.module.ts
+++ mongodb-products-api/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductsModule, ProductModule],
+  imports: [ProductModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
