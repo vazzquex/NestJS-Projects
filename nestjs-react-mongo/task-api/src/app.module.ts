26fe5f7d864124e27e83862940b0408b279deae9
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-10-08 21:03:00 -0300

    add conecction to mongodb

--- nestjs-react-mongo/task-api/src/app.module.ts
+++ nestjs-react-mongo/task-api/src/app.module.ts
@@ -3,8 +3,13 @@ import { TasksModule } from './tasks/tasks.module';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/tasksdb'),
+    TasksModule
+  ],
   controllers: [TasksController],
   providers: [TasksService],
 })
