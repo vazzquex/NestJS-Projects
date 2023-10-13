c112bc7864b515e7a03721f9c5240bc60534086f
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-10-13 19:26:27 -0300

    import taskSchema in task module

--- nestjs-react-mongo/task-api/src/tasks/tasks.module.ts
+++ nestjs-react-mongo/task-api/src/tasks/tasks.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Task, taskSchema } from 'src/schemas/task.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Task.name,
+        schema: taskSchema,
+      },
+    ])
+  ],
   controllers: [TasksController],
   providers: [TasksService]
 })
-export class TasksModule {}
+export class TasksModule { }
