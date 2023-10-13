01b23d3553f9f40f84facb52700bdff993dcfbc7
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-10-13 19:19:33 -0300

    add validator in create task dto

--- nestjs-react-mongo/task-api/src/dto/create.task.dto.ts
+++ nestjs-react-mongo/task-api/src/dto/create.task.dto.ts
@@ -1,5 +1,15 @@
-export interface createTaskDto {
+import {IsString, IsBoolean, IsOptional, IsNotEmpty} from 'class-validator'
+
+export class createTaskDto {
+    @IsString()
+    @IsNotEmpty()
     title: string
+
+    @IsString()
+    @IsOptional()
     description?: string
+
+    @IsBoolean()
+    @IsOptional()
     done?: boolean
 }
