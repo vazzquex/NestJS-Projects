b962a955100c92c41ab13dc12605be2d18a9d778
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-10-18 23:39:40 -0300

    create controllers in task.controller

--- nestjs-react-mongo/task-api/src/tasks/tasks.controller.ts
+++ nestjs-react-mongo/task-api/src/tasks/tasks.controller.ts
@@ -1,10 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private taskService: TasksService) {}
-    
-    
+    constructor(private taskService: TasksService) { }
+
+    @Get()
+    findAll() {
+        return 'Get all tasks';
+    }
+
+    @Get(':id')
+    findById() {
+        return 'Get task by id';
+    }
+
+    @Post()
+    create() {
+        return 'create task';
+    }
+
+    @Delete(':id')
+    delete() {
+        return 'delete task';
+    }
+
 
 }
